ea84f00aa66381a30b6a8e94812b8aed7bc537cf
pedro felipe <pedrofelipinho@gmail.com>
2024-06-24 17:31:58 -0300

    Criando 2 objetos do tipo Musica na classe principal

--- Screen-Sound-2-curso/Program.cs
+++ Screen-Sound-2-curso/Program.cs
@@ -1,4 +1,17 @@
-﻿// nome
-// artista
-// duração
-// disponivel
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Console.WriteLine("***********************");
+
+Musica musica2  = new Musica();
+
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+Console.WriteLine($"Nome: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
