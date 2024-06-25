9e4119e16127f02869331579157115dbece18bda
pedro felipe <pedrofelipinho@gmail.com>
2024-06-25 09:20:36 -0300

    Refatorndo apagando os métodos de leitura e de escrita e colocando o get e o set no Disponivel

--- Screen-Sound-2-curso/Program.cs
+++ Screen-Sound-2-curso/Program.cs
@@ -3,14 +3,14 @@
 musica1.nome = "Roxanne";
 musica1.artista = "The Police";
 musica1.duracao = 187;
-musica1.EscreveDisponivel(true);
+musica1.Disponivel = true;
 
 Musica musica2  = new Musica();
 
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 186;
-musica2.EscreveDisponivel(false);
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
