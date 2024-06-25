88f97b5497bbb86a41fd72ea91e4296bb37b2305
pedro felipe <pedrofelipinho@gmail.com>
2024-06-25 08:58:09 -0300

    Criando os métodos de ler e escrever a disponibilidade da música

--- Screen-Sound-2-curso/Program.cs
+++ Screen-Sound-2-curso/Program.cs
@@ -3,14 +3,14 @@
 musica1.nome = "Roxanne";
 musica1.artista = "The Police";
 musica1.duracao = 187;
-musica1.disponivel = true;
+musica1.EscreveDisponivel(true);
 
 Musica musica2  = new Musica();
 
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 186;
-musica2.disponivel= false;
+musica2.EscreveDisponivel(false);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
