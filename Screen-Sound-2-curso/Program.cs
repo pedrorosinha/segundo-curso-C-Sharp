97b4098980aaf06679e89519d033dbf56857cce1
pedro felipe <pedrofelipinho@gmail.com>
2024-06-25 16:25:30 -0300

    Criando o método de exibir as musicas do album e também mostrar a duração do album

--- Screen-Sound-2-curso/Program.cs
+++ Screen-Sound-2-curso/Program.cs
@@ -21,9 +21,13 @@ albumDoQueen.Nome = "A night at the opera";
 
 Musica musica1 = new Musica();
 musica1.Nome = "Love of my Life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
