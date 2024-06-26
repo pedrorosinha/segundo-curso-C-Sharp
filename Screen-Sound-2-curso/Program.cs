9edea00affd5336ac5ce1c8dffbf62cc1030d31d
pedro felipe <pedrofelipinho@gmail.com>
2024-06-26 16:40:05 -0300

    Criando a classe Podcast relacionando com a classe de episódios e mostrando no arquivo principal

--- Screen-Sound-2-curso/Program.cs
+++ Screen-Sound-2-curso/Program.cs
@@ -41,7 +41,18 @@
 //albumDoQueen.ExibirMusicasDoAlbum();
 //queen.ExibirDiscografia();
 
-Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+Episodio ep1 = new(2, "Técnicas de facilitação", 45);
 ep1.AdicionarConvidados("Maria");
 ep1.AdicionarConvidados("Marcelo");
-Console.WriteLine(ep1.Resumo);
+//Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
+//Console.WriteLine(ep2.Resumo);
+
+Podcast podcast = new("Podcast especial", "Pedro Felipe");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
